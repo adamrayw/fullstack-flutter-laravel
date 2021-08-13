233da3018a946398ac4c5f06098e1ab062379fd8
adamray <adamrayw@users.noreply.github.com>
2021-08-13 11:37:42 +0700

    Add WishlistCard

---
+++ lib/widgets/wishlist_card.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class WishlistCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 20,
+      ),
+      padding: EdgeInsets.only(
+        top: 10,
+        left: 12,
+        bottom: 14,
+        right: 20,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: backgroundColor4,
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              'assets/image_shoes.png',
+              width: 60,
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Terrex Urban Low',
+                  style: primaryTextStyle.copyWith(
+                    fontWeight: semiBold,
+                  ),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  '\$143,98',
+                  style: priceTextStyle,
+                ),
+              ],
+            ),
+          ),
+          Image.asset(
+            'assets/button_wishlist_blue.png',
+            width: 34,
+          ),
+        ],
+      ),
+    );
+  }
+}
