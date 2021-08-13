0942af6a30b8bdf80019d12637d5e7b504dbef8f
adamray <adamrayw@users.noreply.github.com>
2021-08-13 08:54:17 +0700

    Add ChatTile.dart

---
+++ lib/widgets/chat_tile.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class ChatTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 33),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/image_shop_logo.png',
+                height: 54,
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Shoe Store',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      'Good night, This item is on...',
+                      style: secondaryTextStyle.copyWith(
+                        fontWeight: light,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              // Spacer(),
+              Text(
+                'Now',
+                style: secondaryTextStyle.copyWith(
+                  fontSize: 10,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Divider(
+            thickness: 1,
+            color: Color(0xff2B2939),
+          ),
+        ],
+      ),
+    );
+  }
+}
