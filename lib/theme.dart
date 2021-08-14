5f60e3a612eeccea1d0cfc78dd8441525b00949c
adamray <adamrayw@users.noreply.github.com>
2021-08-14 09:38:51 +0700

    Add New Color

--- lib/theme.dart
+++ lib/theme.dart
@@ -12,6 +12,7 @@ Color backgroundColor2 = Color(0XFF2B2937);
 Color backgroundColor3 = Color(0XFF242231);
 Color backgroundColor4 = Color(0XFF252836);
 Color backgroundColor5 = Color(0XFF282844);
+Color backgroundColor6 = Color(0XFFECEDEF);
 Color primaryTextColor = Color(0XFFF1F0F2);
 Color secondaryTextColor = Color(0XFF999999);
 Color subtitleColor = Color(0XFF504F5E);
