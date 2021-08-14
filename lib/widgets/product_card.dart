4b7f2e43961f27f498844695f19983b8ae7143f8
adamray <adamrayw@users.noreply.github.com>
2021-08-14 09:36:46 +0700

    Update Product Card

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -4,63 +4,68 @@ import 'package:shamo/theme.dart';
 class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 215,
-      height: 278,
-      margin: EdgeInsets.only(
-        right: defaultMargin,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Color(0xffECEDEF),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          Image.asset(
-            'assets/image_shoes.png',
-            width: 215,
-            height: 150,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(
-              horizontal: 20,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/product');
+      },
+      child: Container(
+        width: 215,
+        height: 278,
+        margin: EdgeInsets.only(
+          right: defaultMargin,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Color(0xffECEDEF),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 30,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Hiking',
-                  style: secondaryTextStyle.copyWith(
-                    fontSize: 12,
-                  ),
-                ),
-                SizedBox(height: 6),
-                Text(
-                  'COURT VISION 2.0',
-                  style: blackTextStyle.copyWith(
-                    fontSize: 18,
-                    fontWeight: semiBold,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 6),
-                Text(
-                  '\$58,67',
-                  style: priceTextStyle.copyWith(
-                    fontSize: 14,
-                    fontWeight: medium,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
+            Image.asset(
+              'assets/image_shoes.png',
+              width: 215,
+              height: 150,
+              fit: BoxFit.cover,
             ),
-          ),
-        ],
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hiking',
+                    style: secondaryTextStyle.copyWith(
+                      fontSize: 12,
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    'COURT VISION 2.0',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: semiBold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    '\$58,67',
+                    style: priceTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: medium,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
